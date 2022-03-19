c58e2c7fd8e0267c12d758862cf2d2e557c98bbf
ahmed nasser <ahmednas197@gmail.com>
2022-03-19 21:20:50 +0200

    some fixes in Airline controllers

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -2,15 +2,14 @@ module Api
     module v1 
         class AirlinesController < ApplicationController
             def index
-                airlines = Airline.AirlinesController
+                airlines = Airline.all
+            #    // airlines = Airline.AirlinesController
             
                 render json: AirlineSerializer.new(airlines, options).serialized_json
             end
 
-
             def show
                 airline = Airline.find_by(slug: params[:slug])
-
                 render json: AirlineSerializer.new(airline, options).serialized_json
 
             end
@@ -21,7 +20,7 @@ module Api
                 if airline.save
                     render json: AirlineSerializer.new(airline).serialized_json                  
                 else
-                    render json:  {error: airline.errors.message}, status: 422
+                    render json:  {error: airline.errors.messages }, status: 422
                 end
 
             end
@@ -29,12 +28,12 @@ module Api
 
 
             def update 
-                airline = Airline.find_by(slug: params:[:slug])
+                airline = Airline.find_by(slug: params[:slug])
                 
                 if airline.upadte(airline_params)
                     render json: AirlineSerializer.new(airline, options).serialized_json                  
                 else
-                    render json:  {error: airline.errors.message}, status: 422
+                    render json:  {error: airline.errors.messages }, status: 422
                 end
 
             end
@@ -46,7 +45,7 @@ module Api
                 if airline.destroy
                     head :no_content        
                 else
-                    render json:  {error: airline.errors.message}, status: 422
+                    render json:  {error: airline.errors.messages }, status: 422
                 end
 
             end
@@ -55,18 +54,12 @@ module Api
             private
 
             def airline_params
-                params.require(:airline).permit(:name,  :image_url)
+                params.require(:airline).permit(:name, :image_url)
             end
 
             def options
                 @options ||= { include: %i[reviews]}
             end
-
-
-
-
-
-
 
 
         end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   namespace :api do 
-    namspace :v1 do
+    namespace :v1 do
       resources :airlines, param: :slug
       resources :reviews, only: [:create , :destroy]
     end
